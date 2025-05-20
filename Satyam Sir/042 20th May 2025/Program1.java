9ae78855a23ff60d32d5788fd0e4e17f980e7805
SohithReddy <sohithreddy33@gmail.com>
2025-05-20 13:36:22 +0530

    Updated the programs

---
+++ Satyam Sir/042 20th May 2025/Program1.java
@@ -0,0 +1,105 @@
+// Arjun wants to build some homes in a land of size R*C.
+// He wanted to construct homes in rectangular shape.
+// The place which is remained will be used for gradening.
+// Accordingly he has prepared the plan and given as
+// an 2d array plan[][], where 1 indicates home, and 0 indicates garden area.
+
+// A home is set of cells with value 1 in rectangular shape.
+// He wants to findout all the homes in the plan and store their co-ordinates in 
+// the following order, coords[i] = [x1,y1,x2,y2], where (x1,y1) is the starting
+// co-ordinate (top left corner), and (x2,y2) is the ending co-ordinate 
+// (bottom right corner) of i-th home.
+
+// Your task is to help Arjun to find all the homes and return the coords[][] of 
+// all the homes from top left corner to bottom right corner.
+
+// NOTE: No two homes are adjacent to each other in 4 directions,
+// (left, right, top, bottom).
+
+// Input Format:
+// -------------
+// Line-1: Two integers R and C, size of the land.
+// Next R lines: C space separated integers, either 0 or 1
+// 0- represents garden area land and 1- represents the home.
+
+// Output Format:
+// --------------
+// Print 2d array, the co-ordinates of all homes.
+
+
+// Sample Input-1:
+// ---------------
+// 2 3
+// 1 0 0
+// 0 1 1
+ 
+// Sample Output-1:
+// ----------------
+// [0, 0, 0, 0][1, 1, 1, 2]
+
+
+// Sample Input-2:
+// ---------------
+// 4 4
+// 1 1 0 1
+// 0 0 0 0
+// 1 1 0 1
+// 1 1 0 1
+ 
+// Sample Output-2:
+// ----------------
+// [0, 0, 0, 1][0, 3, 0, 3][2, 0, 3, 1][2, 3, 3, 3]
+
+
+
+import java.util.*;
+
+public class Program1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] arr = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        List<int[]> homes = new ArrayList<>();
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (arr[i][j] == 1) {
+                    int x1 = i, y1 = j;
+                    int x2 = i, y2 = j;
+                    while (y2 + 1 < c && arr[x1][y2 + 1] == 1) {
+                        y2++;
+                    }
+                    boolean flag = true;
+                    while (flag && x2 + 1 < r) {
+                        for (int col = y1; col <= y2; col++) {
+                            if (arr[x2 + 1][col] != 1) {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag)
+                            x2++;
+                    }
+                    for (int x = x1; x <= x2; x++) {
+                        for (int y = y1; y <= y2; y++) {
+                            arr[x][y] = 0;
+                        }
+                    }
+
+                    homes.add(new int[] { x1, y1, x2, y2 });
+                }
+            }
+        }
+
+        for (int[] home : homes) {
+            System.out.print(Arrays.toString(home));
+        }
+    }
+}
