2013a0f72b005f4cbdfbe3cac12d81640278e8e9
SohithReddy <sohithreddy33@gmail.com>
2025-06-03 15:20:53 +0530

    Updated Program

--- Satyam Sir/050 3rd June 2025/Notes/jdbc6.java
+++ Satyam Sir/050 3rd June 2025/Notes/jdbc6.java
@@ -2,50 +2,52 @@
 // java -cp "mysql-connector-j-8.0.32.jar;." jdbc6
 
 import java.sql.*;
-import java.time.*;
 import java.util.*;
 
 class jdbc6 {
-  static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-  static final String DB_URL = "jdbc:mysql://localhost/test";
-    
+  static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+  static final String DB_URL = "jdbc:mysql://localhost:3306/test";
   static final String USER = "root";
   static final String PASS = "root";
-     
-  public static void main(String[] args) throws SQLException {  
+
+  public static void main(String[] args) throws SQLException {
     Connection conn = null;
     Statement stmt = null;
     ResultSet rs = null;
     Scanner sc = new Scanner(System.in);
-    try{
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-      stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, 
+    try {
+      Class.forName(JDBC_DRIVER); // Load the driver
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
           ResultSet.CONCUR_READ_ONLY);
 
-      String sql;
-      sql = "SELECT empno from emp order by empno";
-      rs = stmt.executeQuery(sql);  
-      System.out.println(rs.next()); 
-      System.out.println(rs.getInt("empno")); 
-      System.out.println(rs.absolute(2));
-      System.out.println(rs.getInt("empno"));
-      System.out.println(rs.relative(2)); 
-      System.out.println(rs.getInt("empno"));
-      System.out.println(rs.absolute(-1));  // replace with 3 and test
-      System.out.println(rs.getInt("empno"));
-      System.out.println(rs.relative(-1));  // replace with 3 and test
-      System.out.println(rs.getInt("empno"));
+      String sql = "SELECT empno FROM emp ORDER BY empno";
+      rs = stmt.executeQuery(sql);
 
-	
-    }   
-    catch(SQLException e){
+      if (rs.next()) {
+        System.out.println(rs.getInt("empno"));
+      }
+      if (rs.absolute(2)) {
+        System.out.println(rs.getInt("empno"));
+      }
+      if (rs.relative(2)) {
+        System.out.println(rs.getInt("empno"));
+      }
+      if (rs.absolute(-1)) {
+        System.out.println(rs.getInt("empno"));
+      }
+      if (rs.relative(-1)) {
+        System.out.println(rs.getInt("empno"));
+      }
+    } catch (SQLException | ClassNotFoundException e) {
       e.printStackTrace();
-    }
-    finally
-    {
-      rs.close();
-      stmt.close();
-      conn.close();
+    } finally {
+      if (rs != null)
+        rs.close();
+      if (stmt != null)
+        stmt.close();
+      if (conn != null)
+        conn.close();
     }
   }
-}     
+}
